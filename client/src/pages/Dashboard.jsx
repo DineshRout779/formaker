0e7aa6c2d1dbf9ac90b77a35f1e59c86bb253a67
Dinesh Rout <rddinesh779@gmail.com>
2024-02-21 16:44:49 +0530

    style: added welcome hero section

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,10 +3,14 @@ import FormItem from '../components/FormItem';
 import Navbar from '../components/Navbar';
 import { useEffect } from 'react';
 import toast from 'react-hot-toast';
-import { deleteForm, getAllforms } from '../services/form';
+import { createForm, deleteForm, getAllforms } from '../services/form';
+import { useNavigate } from 'react-router-dom';
+import { CircleNotch } from 'phosphor-react';
 
 const Dashboard = () => {
   const [forms, setForms] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
 
   const handleDelete = async (form) => {
     try {
@@ -15,6 +19,19 @@ const Dashboard = () => {
       if (res.status === 200) {
         setForms(forms.filter((f) => f._id !== form._id));
         toast.success('Form deleted');
+      }
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  const createNewForm = async () => {
+    try {
+      const res = await createForm();
+
+      if (res.status === 201) {
+        toast.success('Form created');
+        navigate(`/forms/${res.data.form._id}/edit`);
       }
     } catch (error) {
       console.log(error.response);
@@ -31,6 +48,10 @@ const Dashboard = () => {
         setForms(response.data.forms);
       } catch (err) {
         console.log(err);
+      } finally {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 0);
       }
     };
 
@@ -42,6 +63,49 @@ const Dashboard = () => {
   }, []);
 
   // console.log(forms);
+
+  if (isLoading) {
+    return (
+      <div className='flex justify-center items-center min-h-screen'>
+        <CircleNotch size={32} className='text-purple-600 animate-spin' />
+      </div>
+    );
+  }
+
+  if (forms.length === 0) {
+    return (
+      <div className='min-h-[80vh] flex justify-center text-center items-center flex-col'>
+        <h1 className='text-2xl font-semibold' title='Formaker Logo'>
+          Formaker 🚀
+        </h1>{' '}
+        <h1 className='text-6xl my-8 leading-tight tracking-wide font-extrabold'>
+          Create Seamless Forms, Gather Insights
+        </h1>
+        <div className='flex font-medium items-center gap-2'>
+          <button className='w-[150px] bg-white border-2 border-purple-500 text-purple-500 flex gap-2 items-center justify-center tracking-wider rounded-full hover:shadow-xl p-2 px-6'>
+            Learn more
+          </button>
+          <button
+            onClick={createNewForm}
+            className='w-[150px] bg-purple-500 border-2 border-purple-500 text-white flex gap-2 items-center justify-center tracking-wider rounded-full hover:shadow-xl p-2 px-6'
+          >
+            Try it
+          </button>
+        </div>
+        <p className='fixed bottom-0 mx-auto py-4'>
+          Made by{' '}
+          <a
+            className='text-purple-500'
+            target='_blank'
+            rel='noreferrer'
+            href='https://github.com/DineshRout779'
+          >
+            Dinesh Rout
+          </a>
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div className='container-max'>
